10f15718f04dd2f042212b06f84a703481d0bd98
pedrokpdo <pedromotoehm@gmail.com>
2022-04-12 16:40:15 -0300

    criando componente de permissao

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
 import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
@@ -8,9 +9,6 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
     const { user, isAuthenticated } = useContext(AuthContext)
 
-    const userCanSeeMetrics = useCan({
-        permissions: ['metrics.list']
-    })
 
     useEffect(() => {
         api.get('/me')
@@ -19,9 +17,10 @@ export default function Dashboard() {
 
     return (
         <>
-        <h1>Dashboard: {user?.email}</h1>
-
-    { userCanSeeMetrics && <div>Metricas</div>}
+            <h1>Dashboard: {user?.email}</h1>
+            <Can permissions={['metrics.list']}>
+            <div>Metricas</div>
+            </Can>
         </>
     )
 }
@@ -29,7 +28,7 @@ export default function Dashboard() {
 export const getServerSideProps = withSSRAuth(async (ctx) => {
     const apiClient = setupAPIClient(ctx)
     const response = await apiClient.get('/me')
-     
+
     return {
         props: {}
     }
